9495802973e79f2440bf7b4c2d2d010c5be295d0
Philihp Busby <philihp@gmail.com>
2024-09-26 19:32:40 +0000

    build: update to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['**/dist/**', 'eslint.config.mjs']
+  },
+  {
+    files: ['src/**/*.ts'],
+    extends: [eslint.configs.recommended,
+      ...tseslint.configs.recommended
+    ],
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    }
+  }
+)
